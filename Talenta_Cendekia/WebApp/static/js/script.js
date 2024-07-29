b4ea623022a69151cdbd34424983d7f92cb77ed0
NyxoraXv <87947067+NyxoraXv@users.noreply.github.com>
2024-07-29 19:09:43 +0700

    animation on web revamp

--- Talenta_Cendekia/WebApp/static/js/script.js
+++ Talenta_Cendekia/WebApp/static/js/script.js
@@ -1,13 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log('DOM fully loaded and parsed'); // Debugging
-
     anime({
-        targets: '.container',
+        targets: '.logo',
         opacity: [0, 1], // Fade in from opacity 0 to 1
         translateY: [204, 0], // Move from 204px down to its original position
         easing: 'easeInOutQuad', // Smooth easing function
         duration: 2000 // Animation duration in milliseconds
-    }).finished.then(() => {
-        console.log('Animation finished'); // Debugging
     });
 });
