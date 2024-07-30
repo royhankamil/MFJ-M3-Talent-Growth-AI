47fa9972e3b6711b9cbabd35d67beced784c63e8
NyxoraXv <87947067+NyxoraXv@users.noreply.github.com>
2024-07-31 00:22:40 +0700

    styling landing page

--- Talenta_Cendekia/static/js/script.js
+++ Talenta_Cendekia/static/js/script.js
@@ -3,11 +3,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     anime({
         targets: '.container',
-        opacity: [0, 1], // Fade in from opacity 0 to 1
         translateY: [204, 0], // Move from 204px down to its original position
         easing: 'easeInOutQuad', // Smooth easing function
         duration: 2000 // Animation duration in milliseconds
     }).finished.then(() => {
         console.log('Animation finished'); // Debugging
     });
+});
+
+document.addEventListener("DOMContentLoaded", () =>{
+    anime({
+        targets:'.container',
+        opacity: [0, 1],
+        duration: 6000
+    })
+
 });
