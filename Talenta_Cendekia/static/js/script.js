2f4cad64fe9c350c7c06a8ede9a2c624831249b9
NyxoraXv <87947067+NyxoraXv@users.noreply.github.com>
2024-08-08 14:31:21 +0700

    style

--- Talenta_Cendekia/static/js/script.js
+++ Talenta_Cendekia/static/js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     anime({
         targets: '.batik-pattern',
-        translate: ['120%', '100%'],
+        translate: ['120%', '90%'],
         rotate: ['-10deg', '0deg'],
         opacity : ['0.0', '1.0'],
         filter: ['blur(20px)', 'blur(0px)'],
@@ -20,6 +20,20 @@ document.addEventListener('DOMContentLoaded', function() {
         duration: 2000,
         translateX: ['-60%', '0%'],
         opacity: ['0', '1'],
+        easing: 'easeInOutCubic'
+    })
+    anime({
+        targets: '#tgpattern',
+        top: ['100%','75%'],
+        opacity: ['0', '1'],
+        duration: 2000,
+        easing: 'easeInOutCubic'
+    })
+    anime({
+        targets: '.title',
+        left: ['-60px', '20px'],
+        opacity: ['0', '1'],
+        duration: 2000,
         easing: 'easeInOutCubic'
     })
 });
